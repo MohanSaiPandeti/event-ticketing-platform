c070f5b23b9ff82affd9abdd748c1d2141f85f9e
Mohan Sai <pandetimohansai@gmail.com>
2026-08-19 14:08:25 +0530

    Add EventController for event creation

--- src/main/java/com/mohansai/eventticketing/controller/EventController.java
+++ src/main/java/com/mohansai/eventticketing/controller/EventController.java
@@ -1,4 +1,31 @@
 package com.mohansai.eventticketing.controller;
 
+import com.mohansai.eventticketing.dto.request.CreateEventRequest;
+import com.mohansai.eventticketing.entity.Event;
+import com.mohansai.eventticketing.service.EventService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(
+            @Valid @RequestBody CreateEventRequest request) {
+
+        Event createdEvent = eventService.createEvent(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdEvent);
+    }
 }
